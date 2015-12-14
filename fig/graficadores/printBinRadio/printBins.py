2fa339d5f3da91d9b4565dd1bee9510cfbdbfca1
ppieroni <pepieroni@gmail.com>
2015-12-14 09:04:29 -0300

    bckp 14-12

--- fig/graficadores/printBinRadio/printBins.py
+++ fig/graficadores/printBinRadio/printBins.py
@@ -9,5 +9,8 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
 
 ax.scatter(data[:,0],data[:,1],data[:,2],s=20)
+ax.set_xlabel(r'$\log E_v$',fontsize=20)
+ax.set_ylabel(r'$\theta_D$',fontsize=20)
+ax.set_zlabel(r'${\rm x_d}$',fontsize=20)
 plt.show()
 
